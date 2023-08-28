908670d769f0a2da7e0d6c26c22fa3b193ee4b78
Pedro Duarte <pedrohlduarte@gmail.com>
2023-08-28 19:43:18 -0300

    feat: add reset kratos

---
+++ reset_kratos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+rm -rf dist
+rm -rf node_modules/.cache
+
+rm -rf apps/*/.cache
+rm -rf apps/*/node_modules/.cache
+rm -rf apps/*/node_modules/.vite
+rm -rf apps/*/build
+rm -rf apps/*/public/build
+
+# ui
+find . -type d -name ".vite-storybook" -not -path "**/node_modules/*" -exec rm -rf "{}" \;
+
+# Typescript
+find . -type f -name "tsconfig.tsbuildinfo" -not -path "." -delete
+
+rm -rf apps/*/.turbo
+rm -rf apps/*/node_modules
+rm -rf apps/*/newrelic_agent.log
+
+rm -rf node_modules
+rm -rf pnpm-lock.yaml
+
+pnpm i
+pnpm build
