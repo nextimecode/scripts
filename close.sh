a258404708479f2b400ddde159e8a3edc7ae6eb8
Pedro Duarte <pedrohlduarte@gmail.com>
2023-08-30 16:56:09 -0300

    feat: add close, gather and slack

---
+++ close.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Lista todos os processos em execução e filtra pelo nome da aplicação
+apps=$(osascript -e 'tell application "System Events" to get name of (processes where background only is false)')
+
+# Divide a string em um array separado por vírgulas
+IFS=", " read -ra appArray <<< "$apps"
+
+# Loop para fechar cada aplicação
+for app in "${appArray[@]}"; do
+  osascript -e "tell application \"$app\" to quit"
+done
---
+++ slack.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Import the constants
+source /Users/pedroduarte/Dev/scripts/constants.sh
+
+echo "${INFO}Opening the slack...${RESET}"
+open -a slack
