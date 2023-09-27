7ba44686cd98ea3d93724bf68c23691e0d057540
Pedro Duarte <pedrohlduarte@gmail.com>
2023-09-27 19:35:47 -0300

    fix: change name gpt for help

--- gpt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Import the constants
-source /Users/pedroduarte/Dev/scripts/constants.sh
-
-echo "${INFO}Opening the chatGPT...${RESET}"
-open "/Users/pedroduarte/Applications/Chrome Apps.localized/ChatGPT.app"
---
+++ help.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Import the constants
+source /Users/pedroduarte/Dev/scripts/constants.sh
+
+echo "${INFO}Opening the chatGPT-4...${RESET}"
+open "/Users/pedroduarte/Applications/GPT.app"
