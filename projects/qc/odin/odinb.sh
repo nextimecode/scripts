1c0e76751486c9b9a35b02b7f891f914aaf85114
Pedro Duarte <pedrohlduarte@gmail.com>
2024-01-25 14:22:10 -0300

    feat: add odinb for odin backend

--- projects/qc/odin/odinb.sh
+++ projects/qc/odin/odinb.sh
@@ -1,9 +1,8 @@
 # Import the constants
 source /Users/pedroduarte/Dev/scripts/constants.sh
 
-echo "${INFO}Executing pnpm odin:dev${RESET}"
+echo "${INFO}Executing bundle exec rails s -p 3000${RESET}"
   nvm use 16
   pnpm install
   pnpm build
-  pnpm dev:odin
-
+  bundle exec rails s -p 3000
