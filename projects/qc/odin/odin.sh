70c90d770cd606bc3c8280bbd3575ea19b9a11c4
Pedro Duarte <pedrohlduarte@gmail.com>
2024-04-03 14:57:08 -0300

    feat: add cd in odin projects

--- projects/qc/odin/odin.sh
+++ projects/qc/odin/odin.sh
@@ -4,9 +4,20 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 
-echo "${INFO}Executing pnpm odin:dev${RESET}"
-  nvm use 16
-  pnpm install
-  pnpm build
-  pnpm dev:odin
+cd /Users/pedroduarte/Dev/qc/odin
+
+echo "${INFO}Executing the redis-server${RESET}"
+  osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
+  osascript -e 'tell application "Terminal" to do script "cd /Users/pedroduarte/Dev/qc/odin" in selected tab of the front window'
+  osascript -e 'tell application "Terminal" to do script "redis-server" in selected tab of the front window'
+
+  echo "${INFO}Executing the odin front${RESET}"
+  osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
+  osascript -e 'tell application "Terminal" to do script "cd /Users/pedroduarte/Dev/qc/odin" in selected tab of the front window'
+  osascript -e 'tell application "Terminal" to do script "odinf" in selected tab of the front window'
+
+  echo "${INFO}Executing the odin back${RESET}"
+  osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
+  osascript -e 'tell application "Terminal" to do script "cd /Users/pedroduarte/Dev/qc/odin" in selected tab of the front window'
+  osascript -e 'tell application "Terminal" to do script "odinb" in selected tab of the front window'
 
--- projects/qc/odin/odinf.sh
+++ projects/qc/odin/odinf.sh
@@ -2,13 +2,13 @@
 source /Users/pedroduarte/Dev/scripts/constants.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 
 cd /Users/pedroduarte/Dev/qc/odin
 
-echo "${INFO}Executing bundle exec rails s -p 3001${RESET}"
+echo "${INFO}Executing pnpm odin:dev${RESET}"
   nvm use 16
   pnpm install
-  bundle install
   pnpm build
-  bundle exec rails s -p 3001
+  pnpm dev:odin
+
