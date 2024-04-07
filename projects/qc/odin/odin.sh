ec16350b5ccbecfdcb765624936cdd6cc0776eff
Pedro Duarte <pedrohlduarte@gmail.com>
2024-04-06 23:28:59 -0300

    feat: update odin

--- projects/qc/odin/odin.sh
+++ projects/qc/odin/odin.sh
@@ -1,10 +1,10 @@
+cd /Users/pedroduarte/Dev/qc/odin
+
 # Import the constants
 source /Users/pedroduarte/Dev/scripts/constants.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
-
-cd /Users/pedroduarte/Dev/qc/odin
 
 echo "${INFO}Executing the redis-server${RESET}"
   osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
