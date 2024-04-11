92c34288507e3f72346dc5204cdfb0e4401d8a96
Pedro Duarte <pedrohlduarte@gmail.com>
2024-04-11 07:42:31 -0300

    feat: update backup settings json

--- projects/utils/bootstrap.sh
+++ projects/utils/bootstrap.sh
@@ -11,7 +11,7 @@ MESSAGES=(
   "Protocolos de produtividade ativados. Prepare-se para um dia eficiente!"
   "Verificações de segurança concluídas. Sua estação de desenvolvimento está pronta."
   "Olá, Pedro! Sua agenda de hoje está carregada de possibilidades."
-  "Todos os sistemas operacionais prontos para o dia, Pedro. Qual é o plano?"q
+  "Todos os sistemas operacionais prontos para o dia, Pedro. Qual é o plano?"
   "Sensores indicam um excelente dia para codificar. Vamos nessa?"
   "Boas-vindas, Pedro. Ajustando ambiente para máxima criatividade."
   "Olá, Pedro! Pronto para desafiar os limites do desenvolvimento Front-End hoje?"
