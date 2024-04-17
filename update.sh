206fee326d26ed394471f148f1b7e8225bc79310
Pedro Duarte <pedrohlduarte@gmail.com>
2024-04-17 18:27:44 -0300

    feat: update projetcs

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 # Import the constants
 source /Users/pedroduarte/Dev/scripts/constants.sh
 
-echo "${INFO}Executing pnpm i and pnpm update --latest...${RESET}"
-pnpm i
-pnpm update --latest
+echo "\n${INFO}Executing pnpm update --latest${RESET}"
+  pnpm update --latest
+echo "\n${INFO}Executing pnpm install${RESET}"
+  pnpm i
+echo "\n${INFO}Executing pnpm build${RESET}"
+  pnpm build
