6106b28f751ff42c91d66312d33ee491c0dd2057
Pedro Duarte <pedrohlduarte@gmail.com>
2024-05-30 19:03:18 -0300

    feat: add script create controller

---
+++ libs/nest.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Import the constants
+source /Users/pedroduarte/dev/scripts/constants.sh
+
+# Check if a controller name was provided as an argument
+if [ -z "$1" ]; then
+  echo "\n${ERROR}Controller name is required. Usage: $0 <controller-name>${RESET}"
+  exit 1
+fi
+
+CONTROLLER_NAME=$1
+
+echo "\n${INFO}Executing nest g co ${CONTROLLER_NAME}, create controller${RESET}"
+nest g co $CONTROLLER_NAME
