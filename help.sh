a6c9d299ca3742ee1a6a87211e2679051202c388
Pedro Duarte <pedrohlduarte@gmail.com>
2024-06-07 09:29:48 -0300

    feat: return screipt help

--- help.sh
+++ help.sh
@@ -6,4 +6,9 @@ source /Users/pedroduarte/dev/scripts/constants.sh
 echo "${INFO}Opening the chatGPT-4 in fullscreen mode...${RESET}"
 
 # Abre o aplicativo
-open "/Applications/ChatGPT.app"
+open "/Users/pedroduarte/Applications/gpt.app"
+
+sleep 1
+
+# Usa AppleScript para colocar a aplicação em tela cheia
+osascript -e 'tell application "System Events" to keystroke "f" using {command down, control down}'
