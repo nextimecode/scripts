38443e607a7f956f4c41efe3766ac310e8ee7d90
Pedro Duarte <pedrohlduarte@gmail.com>
2024-06-11 19:44:21 -0300

    feat: add script for duolingo

---
+++ programs/duo.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Import the constants
+source /Users/pedroduarte/dev/scripts/constants.sh
+
+echo "${INFO}Opening the Duolingo...${RESET}"
+open "/Users/pedroduarte/Applications/Duolingo.app"
