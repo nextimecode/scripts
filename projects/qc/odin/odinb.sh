bf3cb5969c65fdd9b2376e36aee974a5d274bce3
Pedro Duarte <pedrohlduarte@gmail.com>
2024-08-03 17:15:56 -0300

    update script odin

--- projects/qc/odin/odinb.sh
+++ projects/qc/odin/odinb.sh
@@ -6,8 +6,8 @@ export NVM_DIR="$HOME/.nvm"
 
 echo "${INFO}Executing cd /Users/pedroduarte/dev/qc/odin${RESET}"
   cd /Users/pedroduarte/dev/qc/odin
-echo "\n${INFO}Executing nvm use 16${RESET}"
-  nvm use 16
+echo "\n${INFO}Executing nvm use 18${RESET}"
+  nvm use 18
 echo "\n${INFO}Executing pnpm install${RESET}"
   pnpm install
 echo "\n${INFO}Executing bundle install${RESET}"
