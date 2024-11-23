eb14d2bc2ce0b294073f8dd14adc0e3bb13aee05
Pedro Duarte <pedrohlduarte@gmail.com>
2024-11-23 17:15:43 -0300

    feat: add custom spell check words and remove outdated playlist file

---
+++ projects/stark/arya.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Import the constants
+source /Users/pedroduarte/dev/scripts/constants.sh
+
+echo "\n${INFO}Executing pnpm install${RESET}"
+  pnpm install
+echo "\n${INFO}Executing pnpm build${RESET}"
+  pnpm build
+echo "${INFO}Executing pnpm arya:dev${RESET}"
+  pnpm arya:dev
