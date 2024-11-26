b42ced8f955edd5386fad25288c7ab2d2fe1ed0e
Pedro Duarte <pedrohlduarte@gmail.com>
2024-11-26 18:21:47 -0300

    fix: adjust spacing in time taken output for better readability in end.sh script

--- libs/end.sh
+++ libs/end.sh
@@ -89,7 +89,7 @@ git push $NO_VERIFY && { printf "${SUCCESS}\n✅ Congratulations, Pedro! Changes
   END_TIME=$(date +%s)
   ELAPSED_TIME=$((END_TIME - START_TIME))
 
-printf "${INFO}${CLOCK_EMOJI} Time taken:${RESET} ${ELAPSED_TIME} seconds.${RESET}\n"
+printf "${INFO}${CLOCK_EMOJI}  Time taken:${RESET} ${ELAPSED_TIME} seconds.${RESET}\n"
 
 # Open the Pull Request page --------------------------------
 
