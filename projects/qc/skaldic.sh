cb482c2fdf6a6b298d8d1db67e047535494b7fcd
Pedro Duarte <pedrohlduarte@gmail.com>
2024-12-08 21:10:59 -0300

    fix: remove redundant pnpm build commands from skaldic scripts

--- projects/qc/skaldic.sh
+++ projects/qc/skaldic.sh
@@ -5,7 +5,5 @@ source /Users/pedroduarte/dev/scripts/constants.sh
 
 echo "\n${INFO}Executing pnpm install${RESET}"
   pnpm install
-echo "\n${INFO}Executing pnpm build${RESET}"
-  pnpm build
 echo "${INFO}Executing pnpm skaldic:dev${RESET}"
   pnpm skaldic:dev
--- projects/qc/skaldic_next.sh
+++ projects/qc/skaldic_next.sh
@@ -5,7 +5,5 @@ source /Users/pedroduarte/dev/scripts/constants.sh
 
 echo "\n${INFO}Executing pnpm install${RESET}"
   pnpm install
-# echo "\n${INFO}Executing pnpm build${RESET}"
-#   pnpm build
 echo "\n${INFO}Executing pnpm skaldic-next:dev${RESET}"
   pnpm skaldic-next:dev
