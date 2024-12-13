0d5dba5ad1492f20757abf40a55d1858e3d446a5
Pedro Duarte <pedrohlduarte@gmail.com>
2024-12-13 18:38:28 -0300

    chore: remove pnpm install command from main.sh script

--- git-commands/main.sh
+++ git-commands/main.sh
@@ -5,5 +5,3 @@ echo "${INFO}Executing git checkout main${RESET}"
   git checkout main
 echo "\n${INFO}Executing git pull origin main${RESET}"
   git pull origin main
-echo "\n${INFO}Executing pnpm install${RESET}"
-  pnpm install
