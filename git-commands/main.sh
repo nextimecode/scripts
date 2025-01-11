d7014827728dc3e639bb27c3c2b5585a81b16fa2
Pedro Duarte <pedrohlduarte@gmail.com>
2025-01-11 18:44:34 -0300

    feat: add cleanup of deleted remote branches in main script

--- git-commands/main.sh
+++ git-commands/main.sh
@@ -5,3 +5,5 @@ echo "${INFO}Executing git checkout main${RESET}"
   git checkout main
 echo "\n${INFO}Executing git pull origin main${RESET}"
   git pull origin main
+echo "\n${INFO}Cleaning up deleted remote branches${RESET}"
+git remote prune origin
