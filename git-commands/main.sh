b75a82aa5cf372e0925cc469a88bab0118244d15
Pedro Duarte <pedrohlduarte@gmail.com>
2025-01-12 18:51:03 -0300

    feat: add cleanup for local branches that no longer exist on remote

--- git-commands/main.sh
+++ git-commands/main.sh
@@ -7,3 +7,6 @@ echo "\n${INFO}Executing git pull origin main${RESET}"
   git pull origin main
 echo "\n${INFO}Cleaning up deleted remote branches${RESET}"
 git remote prune origin
+# Clean up local branches that no longer have a remote
+echo "\n${INFO}Cleaning up local branches that no longer exist on remote${RESET}"
+git branch -vv | grep ': gone]' | awk '{print $1}' | xargs -r git branch -D
